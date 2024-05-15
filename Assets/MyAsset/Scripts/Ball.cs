cc14c8b8dd2b7eab079736d8561ba78b5fc3a828
Shota-Mikami <shota.mikami.5288@gmail.com>
2024-05-15 10:43:33 +0900

    エネミーとボールスクリプト追加
    バグ確認してないんでなんかあったら言ってくれ

---
+++ Assets/MyAsset/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] public float Speed;
+    [SerializeField] public int AttackPower;
+    [SerializeField] public float AirRes;
+
+    //‚Ì‚¿‚É‘Š«’Ç‰Á
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //‘Š«‚É‚æ‚é”j‰óˆ—‚ğŒã‚Å’Ç‰Á
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MyAsset/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] public int HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //後々ステートパターン追加？
+    // Update is called once per frame
+    void Update()
+    {
+        Move(Time.deltaTime);
+        if (HP <= 0)
+        {
+            this.gameObject.AddComponent<Ball>();
+            this.gameObject.tag = "ball";
+            enabled = false;
+        }
+    }
+
+    //絶対継承後追記,移動速度はtimeで制御すること(time = deltaTime)
+    public virtual void Move(float time)
+    {
+
+    }
+
+    //戻り値で相性参照？
+    public void Damage(int damage)
+    {
+        HP -= damage;
+        HP = Mathf.Max(0, HP);
+    }
+}
