be0b1ea1e9df523a1bd821b6fdd99cb32ab32e31
Shota-Mikami <shota.mikami.5288@gmail.com>
2024-06-19 10:54:13 +0900

    Hit音2種、回す音1種実装

--- Assets/MyAsset/Scripts/Rotate.cs
+++ Assets/MyAsset/Scripts/Rotate.cs
@@ -16,10 +16,14 @@ public class Rotate : MonoBehaviour
     [SerializeField] private float rotate_speed_down_rate;  //回転減速係数
     [SerializeField] private float rotate_speed_up_rate;    //回転加速係数
 
-    [SerializeField] private float rot_border_DtoU; //第一～第二象限
-    [SerializeField] private float rot_border_UtoU; //第二～第三象限
-    [SerializeField] private float rot_border_UtoD; //第三～第四象限
+    [SerializeField] private float rot_border_DtoU = 90; //第一～第二象限
+    [SerializeField] private float rot_border_UtoU = 180; //第二～第三象限
+    [SerializeField] private float rot_border_UtoD = 270; //第三～第四象限
+    [SerializeField] private int rotatingSE_num;
     private bool charged;
+    private int point_playSE;      //まわすSEを鳴らしたいところ
+    private int point_rot_now;     //ハンマーの位置
+    private bool checkSE;
 
     private float rotate_speed;
     public float oldspeed_level;
@@ -30,6 +34,8 @@ public class Rotate : MonoBehaviour
     {
         rotate_speed = rotate_speed_init;
         charged = false;
+        point_playSE = 0;
+        checkSE = true;
     }
 
     // Update is called once per frame
@@ -75,21 +81,41 @@ public class Rotate : MonoBehaviour
                     if (transform.eulerAngles.z <= rot_border_DtoU)
                     {
                         rotate_speed *= rotate_speed_down_rate;
+                        if(point_playSE == 0)
+                        {
+                            point_playSE = 1;
+                        }
+                        point_rot_now = 1;
                     }
                     //第二象限で弱く加速
                     else if (transform.eulerAngles.z <= rot_border_UtoU)
                     {
                         rotate_speed += rotate_power / 4.0f;
+                        if (point_playSE == 0)
+                        {
+                            point_playSE = 2;
+                        }
+                        point_rot_now = 2;
                     }
                     //第三象限で大きく加速
                     else if(transform.eulerAngles.z <= rot_border_UtoD)
                     {
                         rotate_speed *= rotate_speed_up_rate;
+                        if (point_playSE == 0)
+                        {
+                            point_playSE = 3;
+                        }
+                        point_rot_now = 3;
                     }
                     //第四象限で減速
                     else
                     {
                         rotate_speed *= rotate_speed_down_rate;
+                        if (point_playSE == 0)
+                        {
+                            point_playSE = 4;
+                        }
+                        point_rot_now = 4;
                     }
 
                     //最大速度なら速度一定に
@@ -99,15 +125,29 @@ public class Rotate : MonoBehaviour
                         charged = true;
                         Debug.Log("Full_Charged");
                     }
+
+                   
                 }
                 else    //最大速度なら速度一定
                 {
                     rotate_speed = rotate_speed_max;
+
+                    if (transform.eulerAngles.z <= rot_border_DtoU) {
+                        point_rot_now = 1;
+                    }
+                    else if (transform.eulerAngles.z <= rot_border_UtoU) {
+                        point_rot_now = 2;
+                    }
+                    else if (transform.eulerAngles.z <= rot_border_UtoD) {
+                        point_rot_now = 3;
+                    }
+                    else {
+                        point_rot_now = 4;
+                    }
                 }
 
+                CheckPlaySEPoint();
                 // startTime = Time.time;
-                
-
 
                 transform.Rotate(new Vector3(0, 0, rotate_speed));
 
@@ -124,6 +164,10 @@ public class Rotate : MonoBehaviour
                 ball.mass = 1.0f;
 
                 charged = false;
+
+                point_playSE = 0;
+
+                checkSE = true;
             }
         }
 
@@ -152,6 +196,21 @@ public class Rotate : MonoBehaviour
         */
     }
 
-
+    private void CheckPlaySEPoint()
+    {
+        if(point_playSE == point_rot_now)
+        {
+            if (!checkSE)
+            {
+                checkSE = true;
+                SoundManager.instance.PlaySE(0);
+            }
+        }
+        else
+        {
+            checkSE = false;
+        }
+        
+    }
 
 }
